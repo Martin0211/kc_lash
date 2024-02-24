dad9aa229112b6b6be45da7355222f4fbeb6cec5
Martin0211 <martin021191@gmail.com>
2024-02-24 16:08:07 -0600

    se configura ruta de post se manejan errores y se acomoda responsividad

--- src/components/ModalSubscribe.jsx
+++ src/components/ModalSubscribe.jsx
@@ -8,6 +8,7 @@ import { IoClose } from "react-icons/io5";
 
 export default function Modal({ isVisible, onClose }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     if (isVisible) {
@@ -71,40 +72,50 @@ export default function Modal({ isVisible, onClose }) {
     }
 
     const isEmailValid = email.trim() === '' || isValidEmail(email);
-    const isPhoneNumberValid = phoneNumber.trim() === '' || isValidPhoneNumber(phoneNumber) ;
+    const isPhoneNumberValid = phoneNumber.trim() === '' || isValidPhoneNumber(phoneNumber);
 
     if (!isEmailValid && !isPhoneNumberValid) {
       window.alert("Ingresa un correo electrónico válido o un número de teléfono válido");
       return;
     }
-  
+
     if (!isEmailValid) {
       window.alert("Correo electrónico no válido");
       return;
     }
-  
+
     if (!isPhoneNumberValid) {
       window.alert("Número de teléfono no válido");
       return;
     }
 
-    const NEXT_PUBLIC_API_URL= process.env.API_URL || "http://localhost:3001";
-    
-    const res = await fetch(`${NEXT_PUBLIC_API_URL}/api/subscribed/`, {
-      method: 'POST',
-      body: JSON.stringify({ names, surname, email, phoneNumber }),
-      headers: {
-        'Content-Type': 'application/json'
+    const NEXT_PUBLIC_API_URL = VERCEL_URL || "http://localhost:3000";
+
+    try {
+      const res = await fetch(`${NEXT_PUBLIC_API_URL}/api/subscribed/`, {
+        method: 'POST',
+        body: JSON.stringify({ names, surname, email, phoneNumber }),
+        headers: {
+          'next-action': 'RENDER'
+        }
+      });
+  
+      if (!res.ok) {
+        throw new Error(`El servidor respondió con el estado ${res.status}`);
       }
-    })
-    const data = await res.json()
-    console.log(data);
-    onClose()
-  }
+  
+      const data = await res.json();
+      console.log(data);
+      onClose();
+    } catch (error) {
+      console.error('Error:', error);
+      setErrorMessage(error.message || 'Se produjo un error.'); // Proporciona un mensaje predeterminado
+    }
+  };
 
   return (
     <div className={`fixed inset-0 Obg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center ${isModalOpen ? 'visible' : 'hidden'}`} id='wrapper' onClick={handleClose}>
-      <div className="w-[600px] m-8 flex flex-col">
+      <div className="w-[400px] md:w-[600px] m-8 flex flex-col">
         <div className="bg-white  p-4 rounded-md">
           <button className="text-black text-xl" onClick={() => onClose()}><IoClose /></button>
           <div className="flex flex-col items-center pb-4">
@@ -118,17 +129,17 @@ export default function Modal({ isVisible, onClose }) {
             <div>
               <h2 className='text-black font-light text-center text-sm pb-3'> Sign up to get exclusive email updates directly from me. </h2>
             </div>
-            <form onSubmit={onSubmit} method = 'POST'>
+            <form onSubmit={onSubmit} method='POST'>
               <div className='flex flex-col p-8 border-2 border-gray-800 rounded-3xl p-[24px]'>
-                <div className='flex'>
-                  <div className='w-[257px] h-[48px] py-[5px] border-2 border-gray-800 rounded-l-3xl'>
+                <div className='md:flex'>
+                  <div className='w-[257px] h-[48px] py-[5px] border-2 border-gray-800 rounded-3xl md:rounded-r-none md:rounded-l-3xl'>
                     <input className='bg-transparent text-black focus:outline-none w-[511px] h-[32px] pl-[16px]'
                       type="text"
                       placeholder="Names"
                       name="names"
                     />
                   </div>
-                  <div className='w-[258px] h-[48px] py-[5px] border-t-2 border-r-2 border-b-2 border-gray-800 rounded-r-3xl'>
+                  <div className='w-[258px] h-[48px] py-[5px] mt-4 md:mt-0 border-2 md:border-l-0 md:border-t-2 md:border-r-2 md:border-b-2 border-gray-800  rounded-3xl md:rounded-l-none md:rounded-r-3xl'>
                     <input className='bg-transparent text-black focus:outline-none w-[511px] h-[32px] pl-[16px]'
                       type="text"
                       placeholder="Surname"
@@ -136,13 +147,13 @@ export default function Modal({ isVisible, onClose }) {
                     />
                   </div>
                 </div>
-                <div className='w-[515px] h-[48px] py-[5px] mt-4 border-2 border-gray-800 rounded-3xl'>
+                <div className='w-[257px] md:w-[515px] h-[48px] py-[5px] mt-4 border-2 border-gray-800 rounded-3xl'>
                   <input className='bg-transparent text-black focus:outline-none w-[511px] h-[32px] pl-[16px]'
                     type="text"
                     placeholder="Email"
                     name="email" />
                 </div>
-                <div className='w-[515px] h-[48px] py-[5px] mt-4 border-2 border-gray-800 rounded-3xl'>
+                <div className='w-[257px] md:w-[515px] h-[48px] py-[5px] mt-4 border-2 border-gray-800 rounded-3xl'>
                   <input
                     className='bg-transparent text-black focus:outline-none w-[511px] h-[32px] pl-[16px]'
                     type="tel"
@@ -151,8 +162,11 @@ export default function Modal({ isVisible, onClose }) {
                     id="phone"
                   />
                 </div>
-                <button className='w-[515px] h-[48px] py-[5px] mt-4 border-2 border-gray-800 rounded-3xl bg-gray-800 text-white' >Subscribe</button>
+                <button className='w-[257px] md:w-[515px] h-[48px] py-[5px] mt-4 border-2 border-gray-800 rounded-3xl bg-gray-800 text-white' >Subscribe</button>
               </div>
+              {errorMessage && (
+              <div className="error-message">{errorMessage}</div>
+            )}
             </form>
           </div>
         </div>
