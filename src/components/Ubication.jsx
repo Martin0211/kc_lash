8a0225f634f45de6a77db12a2ea1d23acdb05e32
Martin0211 <martin021191@gmail.com>
2024-03-13 11:12:17 -0600

    se configura ubicacion

--- src/components/Ubication.jsx
+++ src/components/Ubication.jsx
@@ -1,9 +1,18 @@
+'use client'
 import { FaLocationDot } from 'react-icons/fa6';
 
 export default function ButtonLocation() {
-    return(
-        <button className='flex items-center mb-4 text-white'>
-            <FaLocationDot className='m-1 text-white' /> Queretaro
-        </button>
-    )
+  return (
+    <button
+      className='flex items-center mb-4 text-white'
+      onClick={() => {
+        window.open(
+          'https://www.google.com/maps/dir/?api=1&destination=20.630653531881272, -100.39967062154949',
+          '_blank'
+        );
+      }}
+    >
+      <FaLocationDot className='m-1 text-white' /> Queretaro
+    </button>
+  );
 }
