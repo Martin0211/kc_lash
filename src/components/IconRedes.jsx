bdfe060993f72f02b4c80c1093b5e6fb4b3195f1
Martin0211 <martin021191@gmail.com>
2024-03-13 12:51:22 -0600

    Se ha actualizado la función IcomRedes para que solo reciba el ícono y la ruta como argumentos.

--- src/components/IconRedes.jsx
+++ src/components/IconRedes.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 
-export default function IcomRedes({ icon: Icon, titulo, subtitulo, ruta }) {
+export default function IcomRedes({ icon: Icon, ruta }) {
 
     return (
         <Link href={ruta}>
