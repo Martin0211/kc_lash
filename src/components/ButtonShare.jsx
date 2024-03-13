52b157c224954c2d85eb443b5af1e527071fe787
Martin0211 <martin021191@gmail.com>
2024-03-13 11:09:22 -0600

    se configuro boton para compartir

--- src/components/ButtonShare.jsx
+++ src/components/ButtonShare.jsx
@@ -3,10 +3,27 @@ import React from 'react';
 import { BiShareAlt } from 'react-icons/bi';
 
 export default function ButtonShare() {
-  const handleClick = () => {
+  const handleClick = async () => {
     const currentUrl = window.location.href;
-    console.log('URL actual:', currentUrl);
-    // Aquí puedes realizar más acciones si es necesario
+
+    try {
+      // Verificamos compatibilidad del navegador antes de compartir
+      if (navigator.canShare) {
+        const datosParaCompartir = {
+          url: currentUrl,
+          title: 'Compartir esta página', // Título opcional para el panel para compartir
+        };
+
+        await navigator.share(datosParaCompartir);
+        console.log('¡Página compartida exitosamente!');
+      } else {
+        console.error('La API para compartir no está disponible en este navegador.');
+        // Maneja comportamiento alternativo aquí, si es necesario
+      }
+    } catch (error) {
+      console.error('Error al compartir:', error);
+      // Maneja errores de compartición aquí
+    }
   };
 
   return (
