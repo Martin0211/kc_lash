11a3c197637841ae01b2456a4978264ec5aa1a8c
Martin0211 <martin021191@gmail.com>
2024-03-13 14:32:40 -0600

    se mejora ubicacion

--- src/components/Ubication.jsx
+++ src/components/Ubication.jsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import { FaLocationDot } from 'react-icons/fa6';
 
 export default function ButtonLocation() {
@@ -7,7 +7,7 @@ export default function ButtonLocation() {
       className='flex items-center mb-4 text-white'
       onClick={() => {
         window.open(
-          'https://www.google.com/maps/dir/?api=1&destination=20.631646592133503, -100.399287734269',
+          'https://maps.app.goo.gl/ERqxY5m4qLrQY4Wq5', // Replace with your desired profile URL
           '_blank'
         );
       }}
