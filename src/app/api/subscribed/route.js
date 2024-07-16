3d76670f7503800f108a2b1a8563c554a281797b
Martin0211 <martin021191@gmail.com>
2024-07-16 14:46:45 -0600

    se evita guardar en cache

--- src/app/api/subscribed/route.js
+++ src/app/api/subscribed/route.js
@@ -1,22 +1,27 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
 
+const setNoCacheHeaders = (response) => {
+  response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  response.headers.set('Pragma', 'no-cache');
+  response.headers.set('Expires', '0');
+  return response;
+};
+
 export async function GET(request) {
   try {
     const { rows } = await sql`SELECT * FROM subscribed;`;
-    return NextResponse.json({ subscribers: rows }, { status: 200 });
+    let response = NextResponse.json({ subscribers: rows }, { status: 200 });
+    response = setNoCacheHeaders(response);
+    return response;
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
 }
 
-
 export async function POST(request) {
   const body = await request.json();
-  const names = body.names;
-  const surname = body.surname;
-  const phone_number = body.phone_number;
-  const email = body.email;
+  const { names, surname, phone_number, email } = body;
 
   if (!names || !surname) {
     return NextResponse.json({ error: 'Names and surname are required' }, { status: 400 });
@@ -27,14 +32,54 @@ export async function POST(request) {
   }
 
   try {
-    await sql`INSERT INTO subscribed (names, surname, phone_number, email) VALUES (${names}, ${surname}, ${phone_number}, ${email});`
-
+    await sql`INSERT INTO subscribed (names, surname, phone_number, email) VALUES (${names}, ${surname}, ${phone_number}, ${email});`;
+    const { rows } = await sql`SELECT * FROM subscribed ORDER BY id DESC LIMIT 1;`;
+    const response = NextResponse.json({ subscriber: rows[0] }, { status: 200 });
+    return setNoCacheHeaders(response);
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
+}
 
-  const { rows } = await sql`SELECT * FROM subscribed;`;
-  const lastSubscriber = rows[rows.length - 1];
-  return NextResponse.json({ subscriber: lastSubscriber }, { status: 200 });
+export async function PUT(request) {
+  const url = new URL(request.url);
+  const id = url.searchParams.get('id');
+  const body = await request.json();
+  const { names, surname, phone_number, email } = body;
 
+  if (!id) {
+    return NextResponse.json({ error: 'ID is required' }, { status: 400 });
+  }
+
+  try {
+    await sql`
+      UPDATE subscribed
+      SET names = ${names},
+          surname = ${surname},
+          phone_number = ${phone_number},
+          email = ${email}
+      WHERE id = ${id};
+    `;
+    const response = NextResponse.json({ message: 'Subscriber updated successfully' }, { status: 200 });
+    return setNoCacheHeaders(response);
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
+
+export async function DELETE(request) {
+  const url = new URL(request.url);
+  const id = url.searchParams.get('id');
+
+  if (!id) {
+    return NextResponse.json({ error: 'ID is required' }, { status: 400 });
+  }
+
+  try {
+    await sql`DELETE FROM subscribed WHERE id = ${id}`;
+    const response = NextResponse.json({ message: 'Subscriber deleted successfully' }, { status: 200 });
+    return setNoCacheHeaders(response);
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
 }
