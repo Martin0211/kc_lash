ece2519b88dd7d64cfcd92cdaf29275cf413fa8c
Martin0211 <martin021191@gmail.com>
2024-07-16 16:39:10 -0600

    agregando comfiguracion de Next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        VERCEL_URL: process.env.VERCEL_URL,
-      },
+  env: {
+    VERCEL_URL: process.env.VERCEL_URL,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 };
 
 module.exports = nextConfig;
+
