8774602dd7227dc66fe462d16752d776df825c18
Martin0211 <martin021191@gmail.com>
2024-07-16 19:30:34 -0600

    prueba

--- src/app/dashboard/subscribed/page.jsx
+++ src/app/dashboard/subscribed/page.jsx
@@ -1,6 +1,6 @@
 import SubscribedClient from "@/components/SubscribedClient"
 
-/* const fetchSubscribed = async () => {
+const fetchSubscribed = async () => {
   let NEXT_PUBLIC_API_URL = process.env.VERCEL_URL || "http://localhost:3000/";
   if (!NEXT_PUBLIC_API_URL.endsWith('/')) {
       NEXT_PUBLIC_API_URL += '/';
@@ -14,12 +14,12 @@ import SubscribedClient from "@/components/SubscribedClient"
     }
   });
   return res.json();
-}; */
+};
 
 export default async function SubscribedPage({ params }) {
-  /* const subscribedData = await fetchSubscribed(); */
+  const subscribedData = await fetchSubscribed();
   
   return (
-    <SubscribedClient /* subscribedData={subscribedData} */ />
+    <SubscribedClient subscribedData={subscribedData} />
   );
 }
