d98840a9f9d24e43080b9ead9ba1edba263c04f9
Martin0211 <martin021191@gmail.com>
2024-07-16 21:57:57 -0600

    Prueba URLs

--- src/components/ModalSubscribe.jsx
+++ src/components/ModalSubscribe.jsx
@@ -128,7 +128,7 @@ export default function Modal({ isVisible, onClose }) {
       return;
     }
     
-    const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/";
+    let NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/";
     if (!NEXT_PUBLIC_API_URL.endsWith('/')) {
       NEXT_PUBLIC_API_URL += '/';
     }
