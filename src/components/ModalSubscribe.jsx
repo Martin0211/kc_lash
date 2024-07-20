0eb0fb47d7cd49c21fc5d06356d39cdd751cc286
Martin0211 <martin021191@gmail.com>
2024-07-19 20:43:30 -0600

    Prueba de CORS

--- src/components/ModalSubscribe.jsx
+++ src/components/ModalSubscribe.jsx
@@ -128,7 +128,7 @@ export default function Modal({ isVisible, onClose }) {
       return;
     }
     
-    let NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL;
+    let NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/";
     if (!NEXT_PUBLIC_API_URL.endsWith('/')) {
       NEXT_PUBLIC_API_URL += '/';
     }
