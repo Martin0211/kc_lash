ef0dbf8690cf8224f6cf9dd3bab5e629375448d0
Martin0211 <martin021191@gmail.com>
2024-07-22 16:15:24 -0600

    ruta Url

--- src/components/ModalSubscribe.jsx
+++ src/components/ModalSubscribe.jsx
@@ -128,7 +128,7 @@ export default function Modal({ isVisible, onClose }) {
       return;
     }
     
-    let NEXT_PUBLIC_API_URL = "https://kc-lash.vercel.app" /* || "http://localhost:3000/" */;
+    let NEXT_PUBLIC_API_URL = "https://kc-lash.vercel.app/" /* || "http://localhost:3000/" */;
     console.log(NEXT_PUBLIC_API_URL);
 
     /* if (!NEXT_PUBLIC_API_URL.endsWith('/')) {
