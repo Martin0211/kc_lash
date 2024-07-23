c13dffe8241fab0ffaa14cba2da93a3378266eaa
Martin0211 <martin021191@gmail.com>
2024-07-22 19:24:04 -0600

    Manejando errores

--- src/app/dashboard/subscribed/page.jsx
+++ src/app/dashboard/subscribed/page.jsx
@@ -1,17 +1,37 @@
 import SubscribedClient from "@/components/SubscribedClient"
 
 const fetchSubscribed = async () => {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL
-  
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-  const res = await fetch(`https://${apiUrl}/api/subscribed/`, {
-    headers: {
-      'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
-      'Pragma': 'no-cache',
-      'Expires': '0'
+  try {
+    const res = await fetch(`https://${apiUrl}/api/subscribed/`, {
+      headers: {
+        'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0'
+      }
+    });
+
+    if (!res.ok) {
+      // Manejar errores de respuesta no exitosa
+      console.error(`Error: ${res.status} ${res.statusText}`);
+      throw new Error(`HTTP error! Status: ${res.status}`);
     }
-  });
-  return res.json();
+
+    // Verificar si la respuesta es JSON válida
+    const contentType = res.headers.get('content-type');
+    if (!contentType || !contentType.includes('application/json')) {
+      console.error('Error: Expected JSON response');
+      throw new Error('Expected JSON response');
+    }
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    // Manejar errores de red u otros errores
+    console.error('Failed to fetch subscribed data:', error);
+    throw error; // Vuelve a lanzar el error para que pueda ser manejado por el llamador
+  }
 };
 
 export default async function SubscribedPage({ params }) {
