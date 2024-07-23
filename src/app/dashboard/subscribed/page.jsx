3395b3e720c201472c67ae00074b5d95caff8675
Martin0211 <martin021191@gmail.com>
2024-07-22 18:09:09 -0600

    configurando URL y variables de entorno

--- src/app/dashboard/subscribed/page.jsx
+++ src/app/dashboard/subscribed/page.jsx
@@ -1,9 +1,16 @@
 import SubscribedClient from "@/components/SubscribedClient"
 
 const fetchSubscribed = async () => {
+  let NEXT_PUBLIC_API_URL = process.env.VERCEL_URL || "http://localhost:3000/";
+  if (!/^https?:\/\//i.test(NEXT_PUBLIC_API_URL)) {
+    NEXT_PUBLIC_API_URL = 'https://' + NEXT_PUBLIC_API_URL;  // Asumiendo que Vercel usa HTTPS
+  }
+  
+  if (!NEXT_PUBLIC_API_URL.endsWith('/')) {
+      NEXT_PUBLIC_API_URL += '/';
+    }
 
-  const res = await fetch(`/api/subscribed/`, {
-    method: 'GET',
+  const res = await fetch(`${NEXT_PUBLIC_API_URL}api/subscribed/`, {
     headers: {
       'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
       'Pragma': 'no-cache',
@@ -17,6 +24,6 @@ export default async function SubscribedPage({ params }) {
   const subscribedData = await fetchSubscribed();
   
   return (
-    <SubscribedClient subscribedData={subscribedData} />
+    <SubscribedClient subscribedData={subscribedData}/>
   );
 }
