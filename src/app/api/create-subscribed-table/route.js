63b776d4f3cf3dcfccce7ebecdb771c64c54a51e
Martin0211 <martin021191@gmail.com>
2024-08-02 20:32:04 -0600

    creacion de tabla

--- src/app/api/create-subscribed-table/route.js
+++ src/app/api/create-subscribed-table/route.js
@@ -1,19 +1,17 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
-
+ 
 export async function GET(request) {
   try {
-    // Eliminar esta línea:
-    // const result = await sql`CREATE TABLE IF NOT EXISTS subscribed (
-    //   id SERIAL PRIMARY KEY,
-    //   names VARCHAR(255) NOT NULL,
-    //   surname VARCHAR(255) NOT NULL,
-    //   phone_number VARCHAR(255),
-    //   email VARCHAR(255)
-    // );`;
-
-    // Resto de tu código
-    return NextResponse.json({ message: 'Table creation prevented' }, { status: 200 });
+    const result =
+      await sql`CREATE TABLE IF NOT EXISTS subscribed (
+        id SERIAL PRIMARY KEY,
+        names VARCHAR(255) NOT NULL,
+        surname VARCHAR(255) NOT NULL,
+        phone_number VARCHAR(255),
+        email VARCHAR(255)
+      );`;
+    return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
